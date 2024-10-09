1e2a90febb457e5205560be2dd2fdf02e32a725b
rshdevmachine <152737810+rshdevmachine@users.noreply.github.com>
2024-10-10 00:08:39 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://fitness-tracker-w93p.onrender.com",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
